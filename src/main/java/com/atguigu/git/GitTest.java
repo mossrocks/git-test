d23e554b44fe879b39f7ce8e3a2a2cc372484784
admin <admin@qq.com>
2021-06-30 01:33:18 +0800

    hotfix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello git55!");
         System.out.println("hello git5!");
+        System.out.println("hello git6!");
+        System.out.println("hello git7!");
     }
 }
