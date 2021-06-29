cbb8de2d154632a1313ab145d6dc9ebd10e170f2
admin <admin@qq.com>
2021-06-30 00:50:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Classname GitTest
+ * @Description TODO
+ * @Date 2021/6/30 0:44
+ * @Created by fzl
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
